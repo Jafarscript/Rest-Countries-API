f9f8c71f4a37d4e57efcd1fd1c81a95b671a3854
Jafarscript <lihammedjafar@gmail.com>
2025-02-04 23:48:14 +0100

    building done

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,24 @@
-import { useEffect, useState } from "react";
-import Filters from "./components/Filters";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CountryDetails from "./pages/CountryDetails";
 import NavBar from "./components/NavBar";
-import SearchBar from "./components/SearchBar";
 
 const App = () => {
-  const [countries, setCountries] = useState([]);
-  useEffect(() => {
-    fetch("https://restcountries.com/v3.1/all")
-    .then(res => res.json())
-    .then(data => {
-      setCountries(data)
-      console.log(data)
-    })
-    .catch(err => console.error(err))
-  }, []);
+
+  const toggleDarkMode = () => {
+    document.documentElement.classList.toggle('dark')
+    console.log('Yes')
+  }
   return (
-    <main className="bg-[#fafafa] min-h-[100vh] dark:bg-dark-blue dark:text-white">
-      <NavBar />
-      <section className="px-4 lg:px-24 mt-12 ">
-        <div className="flex flex-wrap gap-4 justify-between items-center">
-          <SearchBar />
-          <Filters />
-        </div>
-       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-16 mt-16">
-       {countries.map(country => (
-        <div key={country.cca2} className="rounded-sm shadow-md overflow-hidden">
-          <img src={country.flags.png} alt={country.name.common} className="h-44 w-full" />
-          <div className="p-4 bg-white dark:bg-dark-blue dark:text-white">
-          <h1 className="font-bold text-lg mb-4">{country.name.common}</h1>
-          <p className="mb-2"><span className="font-medium ">Population:</span> {country.population}</p>
-          <p className="mb-2"><span className="font-medium">Region :</span> {country.region}</p>
-          <p className="mb-2"><span className="font-medium">Capital :</span> {country.capital}</p>
-          </div>
-        </div>
-       ))}
-       </section>
-      </section>
+    <BrowserRouter>
+    <main className="bg-[#fafafa] min-h-[100vh] text-[#111517] dark:bg-[#202c37] dark:text-white">
+      <NavBar toggleDarkMode={toggleDarkMode}/>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/country/:code" element={<CountryDetails />} />
+      </Routes>
     </main>
+    </BrowserRouter>
   );
 };
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,11 @@
 import { BsMoon } from "react-icons/bs"
 
-const NavBar = () => {
+// eslint-disable-next-line react/prop-types
+const NavBar = ({toggleDarkMode}) => {
   return (
-    <nav className="flex justify-between items-center py-4 px-4 lg:px-24 shadow-md bg-white dark:bg-dark-blue text-gray-800 dark:text-white">
+  <nav className="flex justify-between items-center py-4 px-4 lg:px-24 shadow-md bg-white dark:bg-[#2b3945]">
         <h1 className="lg:text-2xl font-bold">Where in the world?</h1>
-        <button className="flex items-center gap-2 text-sm font-medium"><BsMoon  className="font-bold"/> Dark Mode</button>
+        <button onClick={toggleDarkMode} className="flex items-center gap-2 text-sm font-medium cursor-pointer"><BsMoon  className="font-bold"/> Dark Mode</button>
     </nav>
 
   )
